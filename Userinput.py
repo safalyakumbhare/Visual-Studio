956be86eb695200387cf8d66bdd237fc3b9dbbbe
Safalya Kumbhare <safalyakumbhare@gmail.com>
2023-01-11 20:51:15 +0530

    Userinput in Pyhton

---
+++ Userinput.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the number one: "))
+b = int(input("Enter the number two: "))
+print("The Subtraction of ",int(a), "-", int(b) ,"=",int(a)-int(b))
+print("The Multiplication of ",int(a), "*", int(b) ,"=",int(a)*int(b))
+print("The Addition of ",int(a), "+", int(b) ,"=",int(a)+int(b))
+print("The Division of ",int(a), "/", int(b) ,"=",int(a)/int(b))
+
