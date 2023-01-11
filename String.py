493e8de0aad1d46caf9b7d1d862cb08985424e7f
Safalya Kumbhare <safalyakumbhare@gmail.com>
2023-01-11 21:04:27 +0530

    String in Python

---
+++ String.py
@@ -0,0 +1,7 @@
+a = "A string in double quotes"
+b = 'a string in single quotes'
+print(a)
+print(b)
+m = '''this is a multiple line string 
+where you can make multiple line string
+with triple quotes'''
